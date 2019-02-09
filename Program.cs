f769c413b1968b84195ae74cd08ac6ea37119642
Firuzek4 <szymon.firuzek@op.pl>
2019-02-09 11:29:34 +0100

    iwefiwf

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("chuj ci w dupe grubasie");
+            Console.WriteLine("KOCHAM R");
             Console.ReadKey();
             // bal bla bla
         }
